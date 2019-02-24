32ac356d5aab64fa1122fc31db3193777b15d718
Shannan Muench <shmuench@microsoft.com>
2019-02-23 19:50:50 -0800

    Added menu options for user entry

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,13 +6,45 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("************************************");
+            Console.WriteLine("Welcome to the Bank!");
 
-            var myAccount = Bank.CreateAccount("My Checking", "test@test.com", initialDeposit = 1000);
-            Console.WriteLine($"AN: {myAccount.AccountNumber}, Balance: {myAccount.Balance:C}, AT: {myAccount.AccountType}, CD: {myAccount.CreateDate}");
+            while (true)
+            {
+                Console.WriteLine("O. Exit");
+                Console.WriteLine("1. Create an Account");
+                Console.WriteLine("2. Make a Deposit");
+                Console.WriteLine("3. Make a Withdrawal");
+                Console.WriteLine("4. Print All Accounts");
+                Console.WriteLine("5. Print All Transactions");
 
-            var myAccount2 = Bank.CreateAccount("My Savings", "test@test.com", TypeOfAccount.Savings);
-            myAccount2.Deposit(2000);
-            Console.WriteLine($"AN: {myAccount2.AccountNumber}, Balance: {myAccount2.Balance:C}, AT: {myAccount2.AccountType}, CD: {myAccount2.CreateDate}");
+                var option = Console.ReadLine();
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine("Thank you for visiting the bank.");
+                        return;
+
+                    case "1":
+                        Console.Write("Account Name: ");
+                        var accountName = Console.ReadLine();
+                        Console.Write("Email Address :");
+                        var emailAddress = Console.ReadLine();
+                        var myAccount = Bank.CreateAccount(accountName, emailAddress);
+                        Console.WriteLine($"Acount Name: {myAccount.AccountName}, Email Address: {myAccount.EmailAddress}, Account Number: {myAccount.AccountNumber");
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
